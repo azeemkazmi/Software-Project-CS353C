ba3f6d1e15a756339ccd0a7cd133528095c7e94e
azeemkazmi <48208488+azeemkazmi@users.noreply.github.com>
2019-04-19 01:01:04 +0100

    errors

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 //import logo from './logo.svg';
 import './App.css';
 import Authen from './Authen';
+// import Dashboard from './Dashboard';
 
 class App extends Component {
   render() {
@@ -10,6 +11,7 @@ class App extends Component {
         <div className="App-header">
          <h2>Welcome To App</h2>
          <Authen />
+         {/* <Dashboard /> */}
         </div>
        
       </div>
